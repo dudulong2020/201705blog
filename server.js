42ef5129bfffe63dfddc3d0fa0fb865151332391
zhufengnodejs <zhufengnodejs@126.com>
2017-08-13 10:58:35 +0800

    edit

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.set('view engine','html');
 app.set('views',path.resolve('views'));
 //设置html模板的渲染方法
 app.engine('html',require('ejs').__express);
+//参数是静态文件根目录,当客户端访问服务器的静态文件的时候，此中间件会去静态文件根目录下找这个文件，如果找到则返回客户端，找不到则next
+app.use(express.static(path.resolve('node_modules')));
 //如果说请求的URL路径是以/开头的，交给index路由中间件处理
 app.use('/',index);
 //如果说请求的URL路径是以/user开头的，交给user路由中间件处理
