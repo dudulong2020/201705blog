dbaf892e1f8f722e70b073d35777a298cc13591d
zhufengnodejs <zhufengnodejs@126.com>
2017-08-13 10:31:54 +0800

    edit

---
+++ routes/user.js
@@ -0,0 +1,18 @@
+let express = require('express');
+let router = express.Router();
+router.get('/signup',function(req,res){
+  res.send('注册');
+});
+router.post('/signup',function(req,res){
+  res.send('POST 注册');
+});
+router.get('/signin',function(req,res){
+  res.send('登录');
+});
+router.post('/signin',function(req,res){
+  res.send('POST 登录');
+});
+router.get('/signout',function(req,res){
+  res.send('退出');
+});
+module.exports = router;
